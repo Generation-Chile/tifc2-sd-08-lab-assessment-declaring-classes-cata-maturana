ed0547a916af73418c6955bc557435d2c49712cc
cata-maturana <catalina.maturana@genstudents.org>
2024-04-22 17:35:27 -0400

    Tarea realizada

--- task4.js
+++ task4.js
@@ -1,7 +1,17 @@
 export class Player {
-  constructor() {
-   
+  constructor(name, level) {
+   this.name = name
+   this.level = level
+  
   }
-  
-  
+  info() {
+    console.log(`${this.name} has reached Level ${this.level}!`)
+  }
+  levelUp() {
+    this.level++;
+  }
+
 }
+
+const player1 = new Player("Tara", 6)
+console.log(player1)
